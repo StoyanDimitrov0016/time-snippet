ce15602b6711ba84c7a3146ff236d4462b5ae886
StoyanDimitrov0016 <stoyan1020@gmail.com>
2025-12-10 14:22:01 +0200

    refactor: use the new language context, code block component, "Now Object" utils, and the render "Now Object" utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,23 @@
-import { Button } from "@/components/ui/button";
-
-function getISOWeek(date: Date) {
-  // Copy date, force to UTC to avoid DST/timezone surprises
-  const dateUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-
-  // ISO: Monday=1 ... Sunday=7
-  const day = dateUTC.getUTCDay() || 7;
-
-  // Move to Thursday of this week (ISO week-year anchor)
-  dateUTC.setUTCDate(dateUTC.getUTCDate() + 4 - day);
-
-  // Week 1 is the week with Jan 4th
-  const yearStart = new Date(Date.UTC(dateUTC.getUTCFullYear(), 0, 1));
-  const week = Math.ceil(((dateUTC.getTime() - yearStart.getTime()) / 86_400_000 + 1) / 7);
-
-  return week;
-}
+import { getNow } from "./lib/time-utils";
+import { useSecondTimer } from "./hooks/use-second-timer";
+import LanguageSelector from "./components/custom/language-selector";
+import CodeBlock from "./components/custom/code-block";
+import { renderNow } from "./lib/render-utils";
+import { useLanguage } from "./hooks/useLanguage";
 
 export default function App() {
-  const date = new Date();
+  const date = useSecondTimer();
+  const { language } = useLanguage();
 
-  const now = {
-    year: date.getFullYear(),
-    mon: {
-      num: date.getMonth() + 1,
-      name: new Intl.DateTimeFormat(undefined, { month: "long" }).format(date),
-    },
-    week: getISOWeek(date), // your ISO week fn
-    day: {
-      num: ((date.getDay() + 6) % 7) + 1,
-      name: new Intl.DateTimeFormat(undefined, { weekday: "long" }).format(date),
-    },
-    hour: date.getHours(),
-    min: date.getMinutes(),
-    sec: date.getSeconds(),
-  } as const;
+  const now = getNow(date);
+  const code = renderNow(language, now);
+
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <pre>
-        <span>const now = </span>
-        {JSON.stringify(now, null, 2)} <span>as const;</span>
-      </pre>
-    </div>
+    <main className="min-h-screen bg-gray-50 p-6">
+      <div className="max-w-3xl mx-auto">
+        <LanguageSelector />
+        <CodeBlock code={code} language="ts" />
+      </div>
+    </main>
   );
 }
